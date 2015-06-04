fe49d8710c08840c7d0540b367dfb84f631b89ba
Valera Abornev <dr.webmaker@gmail.com>
2015-06-04 15:23:37 +0300

    Make coins green

--- game.js
+++ game.js
@@ -724,6 +724,8 @@ Coin = function () {
               0, -5]);
 
   this.collidesWith = ["ship"];
+  this.color = 'green';
+  this.solid = true;
 
   this.newPosition = function () {
     this.x = Math.random() * Game.canvasWidth;
